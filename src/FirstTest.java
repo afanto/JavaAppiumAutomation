249ab8f688e10128fdc4fcabcfa650a10ed3e575
Andrey Frantov <afanto88@gmail.com>
2018-08-08 23:46:47 +0300

    Initial commit

---
+++ src/FirstTest.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class FirstTest {
+    public void setUp() throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","9");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","");
+    }
+
+}
